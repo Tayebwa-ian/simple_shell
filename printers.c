92195b1099e31d6c68e59e5acc40244f88abbb8d
Big-foot1 <imutyaba11@gmail.com>
2023-10-18 06:14:18 -0700

    printing functions

---
+++ printers.c
@@ -0,0 +1,70 @@
+#include "shell.h"
+/**
+ *print_number - prints numbers to the stdout
+ *@n: number to print
+ *Return: nothing
+ */
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
+
+/**
+ * prompt - Display Shell Prompt
+ */
+void prompt(void)
+{
+	PRINTER("$ ");
+}
+/**
+ * print_error - Display Error Based on Command and How Many Time Shell Looped
+ * @input:User Input
+ * @counter:Simple Shell Count Loop
+ * @argv:Program Name
+ * Return: Void
+ */
+void print_error(char *input, int counter, char **argv)
+{
+	char *er;
+
+	PRINTER(argv[0]);
+	PRINTER(": ");
+	er = _itoa(counter);
+	PRINTER(er);
+	free(er);
+	PRINTER(": ");
+	PRINTER(input);
+	PRINTER(": not found\n");
+}
+
+/**
+ *  _prerror - Print Custome Error
+ * @argv:Program Name
+ * @c:Error Count
+ * @cmd:Command
+ * Return: Void
+ */
+void _prerror(char **argv, int c, char **cmd)
+{
+	char *er = _itoa(c);
+
+	PRINTER(argv[0]);
+	PRINTER(": ");
+	PRINTER(er);
+	PRINTER(": ");
+	PRINTER(cmd[0]);
+	PRINTER(": Illegal number: ");
+	PRINTER(cmd[1]);
+	PRINTER("\n");
+	free(er);
+}
